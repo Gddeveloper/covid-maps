7857b66e395a388aecf55984c9e4d6064dcb6596
Gddeveloper <gouravdas2927@gmail.com>
2020-04-16 19:26:49 +0530

    Update geocode.js

--- server/scripts/geocode.js
+++ server/scripts/geocode.js
@@ -1,7 +1,7 @@
 const geocoder = require("geocoder");
 
 // IP restricted to Arjun's machine
-// const API_KEY = "AIzaSyC4PfZZGA-glhubN7gd4_StbEjrV9LGYBU";
+// const API_KEY = "AIzaSyD61avlRqr82wGusHnDz_j4LpuvYm9ZyGg";
 
 // Make request to
 // https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyC4PfZZGA-glhubN7gd4_StbEjrV9LGYBU&place_id=ChIJWeRNAIBxhlQRfFA7HjhBNjg
